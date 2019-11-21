bc762bce8226fc101d16273a741ee1cf68e688ba
shandy <shandy@dasagroup.co.id>
2019-11-21 09:43:58 +0700

    Modify build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 /etc/init.d/xinetd start
-$FIREBIRD_PATH/bin/gsec -user SYSDBA -password $FIREBIRD_DB_PASSWORD_DEFAULT -modify SYSDBA -pw $FIREBIRD_DB_PASSWORD
 while :
 do
 	echo "[`date`] --> Firebird running..."
